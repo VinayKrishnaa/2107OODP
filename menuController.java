063f57429cd770eb998e2e72a389082c4dd7929d
electriax <85962635+electriax@users.noreply.github.com>
2022-04-09 23:45:23 +0800

    Update menuController.java

--- menuController.java
+++ menuController.java
@@ -7,14 +7,14 @@ import java.util.ArrayList;
 
 public class menuController{
 	ArrayList<menuItem> menuItems;
-    ArrayList<menuItem> orders;
+    ArrayList<OrderItem> orders;
     public int numMenuItem = 0;
     public static menuController instance = null;
     
     // initialize menu and order array list
     public menuController() {
     	menuItems = new ArrayList<menuItem>();
-    	orders = new ArrayList<menuItem>();
+    	orders = new ArrayList<OrderItem>();
 	}
     
     // function to create instance of the constructor menuController
@@ -25,11 +25,10 @@ public class menuController{
     }
 
     // update all variables of selected menu item 
-	public void updateMenu(int index, String name, String description, float price, int preparationTime) {
+	public void updateMenu(int index, String name, String description, float price) {
 		menuItems.get(index).setName(name);
 		menuItems.get(index).setPrice(price);
 		menuItems.get(index).setDescription(description);
-		menuItems.get(index).setPreparationTime(preparationTime);
     }
 
 	// remove an item from the menu
@@ -39,20 +38,26 @@ public class menuController{
     }
 
 	// add an item into the menu
-    public void createItem(String name, String description, float price, int preparationTime){
-    	menuItems.add(new menuItem(name, description, price, preparationTime));
+    public void createItem(String name, String description, float price){
+    	menuItems.add(new menuItem(name, description, price));
     	numMenuItem++;
     }
 
     // select an item from the menu and add it into order list
-    public void orderItem(int index, int roomID){
-    	orders.add(menuItems.get(index));
+    public void orderItem(int index, int roomID,  String remarks){
+    	orders.add(new OrderItem(menuItems.get(index).getName(), menuItems.get(index).getPrice(), roomID, "Confirmed", remarks));
+    	OrderDB.getInstance().saveData();
     }
     
     // prints out current running orders 
     public void currentOrders(){
     	System.out.println(orders);
     }
+    
+ // remove an item from the order list
+ 	public void removeOrder(int index){
+ 		orders.remove(index);
+     }
     
     // prints out menu 
     /*public void printMenu() {
@@ -73,5 +78,22 @@ public class menuController{
 			    				"Preparation Time: " + arr.get(index).getPreparationTime() + "mins}\n");
     	}
     }
-   
+
+	// update status of order
+	public void updateOrder(int index, int status) {
+		switch(status){
+			case 1:
+				orders.get(index).setStatus("Confirmed");
+				break;
+			case 2:
+				orders.get(index).setStatus("Preparing");
+				break;
+			case 3:
+				orders.get(index).setStatus("Delivered");
+				break;
+			default:
+				System.out.println("Please enter a valid option!");
+				break;
+		}
+	}
 }
